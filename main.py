258cfb29337d5f54ab8039e08fbf842f2fdda245
Iman <imanbek.mashrapov@gmail.com>
2025-02-16 11:52:32 +0600

    Smartphone subclass created

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+from device import Device
+from smartphone import Smartphone
+
+
 if __name__ == '__main__':
-    print('Hello world')
+    phone1 = Smartphone('Samsung', 500, 10, '10*10', 3000, warranty_period=6)
+    print(phone1.display_info())
